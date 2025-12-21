26fdd0c74d70721a79c7ebd6d1c7a22137cc8ecc
nabeel nizar <nabeelnizar786@gmail.com>
2025-12-21 23:35:36 +0530

    feat: integrate Redux for user management and add routing
    - Added Redux Toolkit for state management with user slice.
    - Implemented routing using React Router for navigation.
    - Created components for Body, Footer, Login, NavBar, Profile, and Feed.
    - Integrated Tailwind CSS and DaisyUI for styling.
    - Updated package.json with new dependencies.
    - Refactored App component to include routing and Redux Provider.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-function App() {
+import { BrowserRouter, Route, Routes } from "react-router";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import { store } from "./utils/appStore";
+import { Provider } from "react-redux";
+import Feed from "./components/feed";
 
+function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Provider store={store}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,69 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router";
+import { BASE_URL } from "../utils/constants";
+
+const Login = () => {
+  const [emailId, setEmailId] = useState("billupk@gmail.com");
+  const [password, setPassword] = useState("Billu@123");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      let result = await axios.post(
+        BASE_URL + "/login",
+        {
+          emailId,
+          password,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+      dispatch(addUser(result.data));
+      return navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card card-border bg-base-300 w-96">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div>
+            <fieldset className="fieldset my-2">
+              <legend className="fieldset-legend">Email ID</legend>
+              <input
+                type="text"
+                value={emailId}
+                className="input"
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+            </fieldset>
+            <fieldset className="fieldset my-2">
+              <legend className="fieldset-legend">Password</legend>
+              <input
+                type="text"
+                value={password}
+                className="input"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </fieldset>
+          </div>
+          <div className="card-actions justify-center my-2">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
